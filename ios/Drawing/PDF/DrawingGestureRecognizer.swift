066a743866658ae18617b046282e25258a057221
mobileapps23 <40164951+mobileapps23@users.noreply.github.com>
2023-09-19 00:54:36 +0300

    fix: new gesture scheme for PDF view

--- ios/Drawing/PDF/DrawingGestureRecognizer.swift
+++ ios/Drawing/PDF/DrawingGestureRecognizer.swift
@@ -5,47 +5,57 @@
 
 import UIKit
 
-protocol DrawingGestureRecognizerDelegate: AnyObject {
-    func gestureRecognizerBegan(_ location: CGPoint)
-    func gestureRecognizerMoved(_ location: CGPoint)
-    func gestureRecognizerEnded(_ location: CGPoint)
-}
+
 
 class DrawingGestureRecognizer: UIGestureRecognizer {
     weak var drawingDelegate: DrawingGestureRecognizerDelegate?
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first,
-            // touch.type == .pencil, // Uncomment this line to test on device with Apple Pencil
-            let numberOfTouches = event?.allTouches?.count,
-            numberOfTouches == 1 {
-            state = .began
+        if let numberOfTouches = event?.allTouches?.count,
+           numberOfTouches == 1 {
+            if let touch = touches.first
+               // touch.type == .pencil, // Uncomment this line to test on device with Apple Pencil
+                {
+                state = .began
 
-            let location = touch.location(in: self.view)
-            drawingDelegate?.gestureRecognizerBegan(location)
-        } else {
-            state = .failed
+                let location = touch.location(in: self.view)
+                drawingDelegate?.gestureRecognizerBegan(location)
+                print("**", touch.gestureRecognizers)
+            } else {
+                state = .failed
+            }
         }
     }
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        state = .changed
+        if let numberOfTouches = event?.allTouches?.count,
+           numberOfTouches == 1 {
+            state = .changed
 
-        guard let location = touches.first?.location(in: self.view) else { return }
-        drawingDelegate?.gestureRecognizerMoved(location)
+            guard let location = touches.first?.location(in: self.view) else { return }
+            drawingDelegate?.gestureRecognizerMoved(location)
+        }
+
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let location = touches.first?.location(in: self.view) else {
+        if let numberOfTouches = event?.allTouches?.count,
+           numberOfTouches == 1 {
+            guard let location = touches.first?.location(in: self.view) else {
+                state = .ended
+                return
+            }
+            drawingDelegate?.gestureRecognizerEnded(location)
             state = .ended
-            return
         }
-        drawingDelegate?.gestureRecognizerEnded(location)
-        state = .ended
+
     }
 
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
-        state = .failed
+        if let numberOfTouches = event.allTouches?.count,
+           numberOfTouches == 1 {
+            state = .failed
+        }
     }
 }
 
